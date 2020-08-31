cfb5b4f1b0db3a018600a0de8c81de3b285ed9fd
dbc2201 <divyanshb22@gmail.com>
2020-08-31 13:55:08 +0530

    initial commit

---
+++ app/src/androidTest/java/gla/ac/in/miniproject1/stufun/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package gla.ac.`in`.miniproject1.stufun
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("gla.ac.in.miniproject1.stufun", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/gla/ac/in/miniproject1/stufun/MainActivity.kt
@@ -0,0 +1,11 @@
+package gla.ac.`in`.miniproject1.stufun
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
